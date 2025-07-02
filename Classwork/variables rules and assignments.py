a4d28e3761c2f27de57c60312de961d285d53205
haarika-5 <eluruharika267@gmail.com>
2025-07-02 16:46:39 +0530

    variables rules and assignments

---
+++ Classwork/variables rules and assignments.py
@@ -0,0 +1,50 @@
+myvar=10
+print(myvar)
+myVar=10
+print(myVar)
+my_var=10
+print(my_var)
+_myvar=10
+print(_myvar)
+
+#case-sensitive
+Myvar=10
+myvar=100
+print(Myvar)
+print(myvar)
+
+#Basic Assignment
+a=10
+b=20
+print(a)
+print(b)
+
+#Multiple Assignment
+a,b,c=10,20,30
+print(a)
+print(b)
+print(c)
+
+#Same Value Assignment
+a=b=c=100
+print(a)
+print(b)
+print(c)
+
+#Swapping Assignment
+a=5
+b=6
+a,b=b,a
+print(a)
+print(b)
+
+#Deleting Assignment
+a=10
+b=20
+del(a)
+print(b)
+
+a=3
+b=4
+del(b)
+print(a)
