e80804bef8693c835ccb44bd90fafa038006ae91
haarika-5 <eluruharika267@gmail.com>
2025-07-02 17:13:40 +0530

    data types

---
+++ Classwork/data types.py
@@ -0,0 +1,71 @@
+#Integer Type
+a=(20)
+b=30
+print(a,b)
+print(type(a))
+print(type(b))
+
+#Floating point
+a=5.2
+b=10.5
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
+#Complex value
+x=4+6j
+y=2+4j
+print(x,y)
+print(type(x))
+print(type(y))
+
+#String type
+m='hai'
+n="hello"
+o='''hlo'''
+print(m,n,o)
+print(type(m))
+print(type(n))
+print(type(o))
+
+l=list()
+i=[1,2,3,4,5.6]
+print(i)
+print(type(i))
+
+#Tuple type
+t=tuple()
+p=(1,2,3,4)
+print(p)
+print(type(p))
+
+#Set type
+s=set()
+m={10,20,30,40}
+print(m)
+print(type(m))
+
+#Frozenset type
+z=frozenset({10,20,30,40})
+print(z)
+print(type(z))
+
+#Dictionary type
+d=dict()
+g={'name':"harika",'age':"21"}
+print(g)
+print(type(g))
+
+#Boolen type
+u=True
+v=False
+
+print(u,v)
+print(type(u))
+print(type(v))
+
+#None type
+d=None
+print(d)
+print(type(d))
