e5717652c83a794e4e4d11e4f713095a9d65330c
haarika-5 <eluruharika267@gmail.com>
2025-07-05 19:36:08 +0530

    Strings

---
+++ Classwork/Strings.py
@@ -0,0 +1,64 @@
+#String
+Name="Harika"
+print(type(Name))
+
+#1.String Operations
+#Concatenation
+fname='Eluru'
+lname='Harika'
+print(fname+lname)
+
+#Repetition
+name="Pavan"
+print("Pavan"*5)
+
+#Indexing
+text="Pavankumar"
+print(text[3])
+print(text[0])
+print(text[-5])
+print(text[-5])
+
+#Slicing
+name="Srilekha"
+print(name[0:5])
+print(name[:8])
+print(name[3:])
+print(name[:])
+
+#Membership
+name="Harika"
+print('Hari' in name)
+print('Hari' not in name)
+print('kumar' in name)
+print('kumar' not in name)
+
+#2.String Functions
+#len()
+name="Harika"
+print(len(name))
+
+#max() and min()
+name="pavanXYZ"
+print(max("chinnuXYZ"))
+print(min("chinnuXYZ"))
+
+#Sorted()
+name="chinnu"
+print(sorted("chinnu"))
+
+#chr() and ord()
+X=('A')
+print(ord(X))
+X=('a')
+print(ord(X))
+X=('@')
+print(ord(X))
+#chr()
+print(chr(97))
+print(chr(45))
+print(chr(81))
+
+
+
+
