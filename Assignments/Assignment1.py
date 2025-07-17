6006a3bc94220fc776090731e4a0ec12be3dbce7
haarika-5 <eluruharika267@gmail.com>
2025-07-17 19:14:46 +0530

    secondary school management system

---
+++ Assignments/Assignment1.py
@@ -0,0 +1,43 @@
+#Secondary School Management System
+
+#Collecting information of secondary school
+School_name = input("Enter name of the school:")
+Classes_available = input("Enter available classes:")
+available_seats_in_class_8= int(input("Enter available seats in class 8:"))
+available_seats_in_class_9 = int(input("Enter available seats in class 9:"))
+available_seats_in_class_10 = int(input("Enter available seats in class 10:"))
+reserved_seats_in_class_8 = int(input("Enter reserved seats in class 8:"))
+reserved_seats_in_class_9 = int(input("Enter reserved seats in class 9:"))
+reserved_seats_in_class_10 = int(input("Enter reserved seats in class 10:"))
+fee_for_class_8 = int(input("Enter fee for class 8:"))
+fee_for_class_9 = int(input("Enter fee for class 9:"))
+fee_for_class_10 = int(input("Enter fee for class 10:"))
+principal_name = input("Enter prinipal name:")
+contact_details = int(input("Enter contact details:"))
+location = input("Enter location:")
+
+#Displaying the information of secondary school management system
+
+print("\n-- Secondary school management system ---\n")
+
+print("school name:",School_name)
+print("classes available:",Classes_available)
+print("available seats in class 8:",available_seats_in_class_8)
+print("available seats in class 9:",available_seats_in_class_9)
+print("available seats in class 10:",available_seats_in_class_10)
+print("reserved seats in class 8:",reserved_seats_in_class_8)
+print("reserved seats in class 9:",reserved_seats_in_class_9)
+print("reserved seats in class 10:",reserved_seats_in_class_10)
+print("fee for class 8:",fee_for_class_8)
+print("fee for class 9:",fee_for_class_9)
+print("fee for class 10:",fee_for_class_10)
+print("principal name",principal_name)
+print("contact details",contact_details)
+print("location",location)
+
+
+
+
+
+
+
